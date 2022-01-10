d046d38d2aad658f81ea5a7f667e8c3246962e46
Chiamaka Ugoji <mychiamaka@gmail.com>
2022-01-10 10:50:40 +0100

    Added form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Form, Button} from 'react-bootstrap'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container-fluid div-wrapper'>
+        
+        <div className='form-group'>
+      <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email address" />
+    <Form.Text className="text-muted">
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label> Enter Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label> Confirm Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+</div>
+</div>
     </div>
   );
 }
