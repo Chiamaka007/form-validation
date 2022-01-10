2b8f2b5be254d27c6c186a54c290924d5a8eb9af
Chiamaka Ugoji <mychiamaka@gmail.com>
2022-01-10 12:14:52 +0100

    added form validation and styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Form, Button} from 'react-bootstrap'
+import Form from './Form';
 import './App.css';
 
 function App() {
@@ -7,30 +7,8 @@ function App() {
     <div className="App">
       <div className='container-fluid div-wrapper'>
         
-        <div className='form-group'>
-      <Form>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email address" />
-    <Form.Text className="text-muted">
-    </Form.Text>
-  </Form.Group>
-
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label> Enter Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label> Confirm Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
-</div>
+      <Form />
+        
 </div>
     </div>
   );
